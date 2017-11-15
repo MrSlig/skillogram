a33978826d74bf0a4c98ecda4ff84911dc5394b7
SkillUP Student <student@skillup.spb.ru>
2017-11-15 20:56:15 +0300

    lesson 3 PHP & font added

---
+++ PHP/index.php
@@ -0,0 +1,20 @@
+<?php
+echo 'hello world';
+echo '<br/>';
+echo date('Y-m-d H:i:s');
+echo '<br/>';
+echo date('l - z - H:i',time());
+echo '<br/>';
+echo date('Y-m-d H:i:s', time() - 60*60*24);
+echo '<br/>';
+//phpinfo();
+/* 
+ * LESSON 3
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ * использовать для header & footer:
+ * require(filename);
+ * 
+ */
